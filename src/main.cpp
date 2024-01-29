38562e348d988619c913e48e59a62ab3a18c3cbd
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2024-01-30 00:23:43 +0300

    Need to fix this code

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,32 @@
 
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
-const int IMAGE_WIDTH = 256;
-const int IMAGE_HEIGHT = 256;
-const std::string FILE_OUTPUT = "../image.ppm";
+constexpr auto ASPECT_RATIO = 16.0 / 9.0;
+constexpr int IMAGE_WIDTH = 400;
+// Calculate the image height. //No check for if lower than 1
+constexpr int IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
 
+constexpr std::string FILE_OUTPUT = "../image.ppm";
 
 int main() {
-    std::ofstream file_stream; // Open the file and reset its contents
+    // Open the file and reset its contents
+    std::ofstream file_stream;
     file_stream.open(FILE_OUTPUT, std::ios::out | std::ios::trunc);
+    //Camera values
+    auto focal_length = 1.0;
+    auto camera_center = point3(0, 0, 0);
+    constexpr auto viewport_height = 2.0;
+    constexpr auto viewport_width = viewport_height * (static_cast<double>(IMAGE_WIDTH)/IMAGE_HEIGHT);
 
-	file_stream << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
+    auto viewport_u = vec3(viewport_width, 0, 0); //GLSL uv Coords
+    auto viewport_v = vec3(0, -viewport_height, 0); //GLSL uv Coords
+
+    auto pixel_delta_u = viewport_u / IMAGE_WIDTH;
+    auto pixel_delta_v = viewport_v / IMAGE_HEIGHT;
+
+	file_stream << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n"; //Magic bits for ppm
     for (int j = 0; j < IMAGE_HEIGHT; ++j) {
         std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - j) << ' ' << std::flush;
         for (int i = 0; i < IMAGE_WIDTH; ++i) {
@@ -23,4 +38,8 @@ int main() {
     }
     std::clog << "\rCompleted.               \n";
     file_stream.close();
+}
+
+color ray_color(const ray& r) {
+    return color(0, 112, 221);
 }
