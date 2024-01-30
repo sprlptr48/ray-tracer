a4e63bc0d093e976d5a31523924a4413eef7cfca
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2024-01-30 23:01:13 +0300

    Print Normals
    https://raytracing.github.io/books/RayTracingInOneWeekend.html#surfacenormalsandmultipleobjects/simplifyingtheray-sphereintersectioncode

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,27 @@ constexpr int IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
 
 const std::string FILE_OUTPUT = "../image.ppm";
 
+/* (P(t)−C)⋅(P(t)−C)=r² →⇒ t²b⋅b+2tb⋅(A−C)+(A−C)⋅(A−C)−r²=0
+ * Discrim for t var, P(t) = A+bt calc from ray.h, C Centre of Circle*/
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b*b - 4*a*c;
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant) ) / (2.0*a);
+    }
+}
+
 color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r); //ray.h "at"t
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.9, 0.5, 0.9); // linear interp
@@ -35,8 +55,7 @@ int main() {
     auto pixel_delta_v = viewport_v / IMAGE_HEIGHT; // GLSL current UV height
 
     // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
 	file_stream << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n"; //Magic bits for ppm
