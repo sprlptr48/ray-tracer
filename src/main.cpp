d13a09ce79dbd8582d3a28f0c4045f02202c8425
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2024-02-02 01:28:29 +0300

    add rtweekend.h, utility class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <fstream>
 
+#include "rtweekend.h"
 #include "vec3.h"
 #include "color.h"
 #include "ray.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 constexpr auto ASPECT_RATIO = 16.0 / 9.0;
 constexpr int IMAGE_WIDTH = 400;
@@ -12,26 +16,10 @@ constexpr int IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
 
 const std::string FILE_OUTPUT = "../image.ppm";
 
-/* (P(t)−C)⋅(P(t)−C)=r² →⇒ t²b⋅b+2tb⋅(A−C)+(A−C)⋅(A−C)−r²=0
- * Discrim for t var, P(t) = A+bt calc from ray.h, C Centre of Circle*/
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant) ) / a;
-    }
-}
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r); //ray.h "at"t
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record hitrec;
+    if(world.hit(r, 0, infinity, hitrec)) {
+        return 0.5 * (hitrec.normal + color(1, 1, 1));
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
@@ -58,6 +46,11 @@ int main() {
     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
+    //Create a world with two spheres for now
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    world.add(make_shared<sphere>(point3(-15,5.5,-15), 4));
 	file_stream << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n"; //Magic bits for ppm
     for (int j = 0; j < IMAGE_HEIGHT; ++j) {
         std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - j) << ' ' << std::flush;
@@ -66,7 +59,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(file_stream, pixel_color);
         }
     }
---
+++ src/rtweekend.h
@@ -0,0 +1,25 @@
+//
+// Created by eee on 02/02/2024.
+//
+
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+//General Definitions and tools for use
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+const double infinity = std::numeric_limits<double>::infinity();
+constexpr double pi = 3.1415926535897932384626433832795028841971693;
+
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+#endif //RTWEEKEND_H
