b71de8d0e745929e7912da0511ccfb26853ccf28
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2024-02-02 01:04:45 +0300

    Add Hittables and hittable list

---
+++ src/hittable.h
@@ -0,0 +1,35 @@
+//
+// Created by eee on 01/02/2024.
+//
+
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+class hit_record {
+public:
+    point3 p;
+    vec3 normal;
+    double t;
+    bool front_face;
+
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+        // TODO: Turn this "normals based on rays" to "normals always point out"
+
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+class hittable {
+public:
+    virtual ~hittable() = default;
+
+    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+
+};
+
+#endif //HITTABLE_H
---
+++ src/sphere.h
@@ -0,0 +1,46 @@
+//
+// Created by eee on 01/02/2024.
+//
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+
+class sphere : public hittable {
+public:
+    sphere(const point3& _center, const double _radius): center(_center), radius(_radius) {}
+
+    // Return if the ray hits the hit record
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        vec3 oc = r.origin() - center;
+        auto a = r.direction().length_squared();
+        auto half_b = dot(oc, r.direction());
+        auto c = oc.length_squared() - radius*radius;
+        auto discriminant = half_b*half_b - a*c;
+        if (discriminant < 0) return false;
+
+        auto sqrtd = sqrt(discriminant);
+        auto root = (-half_b - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root) {
+            root = (-half_b + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root)
+                return false;
+        }
+
+        rec.p = r.at(root); // root is our new t
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+        rec.t = root;
+
+        return true;
+    }
+
+
+private:
+    point3 center;
+    double radius;
+
+};
+
+#endif //SPHERE_H
