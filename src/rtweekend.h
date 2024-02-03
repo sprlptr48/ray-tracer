800d3ba33ab73862862b8f4bb92e94b6d1722008
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2024-02-03 15:38:52 +0300

    Move camera to its own class. Main is empty now

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -17,6 +17,7 @@ using std::sqrt;
 const double infinity = std::numeric_limits<double>::infinity();
 constexpr double pi = 3.1415926535897932384626433832795028841971693;
 
+const std::string FILE_OUTPUT = "../image.ppm";
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
