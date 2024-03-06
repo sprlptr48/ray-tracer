8f60d5ec673e3b27737acd63d9631b1558f5a2b6
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2024-03-06 11:15:11 +0300

    Camera Movement

--- src/camera.h
+++ src/camera.h
@@ -17,11 +17,15 @@
 
 class camera {
 public:
-    char camName = ' '; // Camera name for debug
-    double aspect_ratio = 1.0; // init to random value before actually changing by user
+    char camName = ' ';         // Camera name for debug
+    double aspect_ratio = 1.0;  // init to random value before actually changing by user
     int    image_width  = 10;
     int    samples_per_pixel = 10;
-    int    max_depth = 10; // Max number of bounces
+    int    max_depth = 10;      // Max number of bounces
+    double fov = 90;            // Vertical Field of View (View Angle)
+    point3 lookfrom = point3(0, 0, -1);
+    point3 lookat = point3(0, 0, 0);
+    vec3 vup = vec3(0, 1, 0); // Camera relative up direction
 
     camera() = default; // we use initialize in code so not needed
 
@@ -54,6 +58,7 @@ private:
     point3 pixel00_loc;         // Location of pixel 0, 0
     vec3   pixel_delta_u;       // Offset to pixel to the right
     vec3   pixel_delta_v;       // Offset to pixel below
+    vec3 u, v, w;               // Camera basis vectors
     std::ofstream file_stream;  // Output file to write to
 
     void initialize() {
@@ -63,16 +68,22 @@ private:
         image_height = static_cast<int>(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
-        center = point3(0, 0, 0); // Camera location
+        center = lookfrom; // Camera location
 
         // Determine viewport dimensions.
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto focal_length = (lookfrom - lookat).length();
+        auto theta = degrees_to_radians(fov);
+        auto h = tan(theta/2);
+        auto viewport_height = 2.0 * h * focal_length;
         auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
 
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        auto viewport_u = viewport_width * u;
+        auto viewport_v = viewport_height  * -v;
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
         pixel_delta_u = viewport_u / image_width;
@@ -80,7 +91,7 @@ private:
 
         // Calculate the location of the upper left pixel.
         auto viewport_upper_left =
-            center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+            center - focal_length*w - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,13 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.325);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), .5);
+    auto material_left   = make_shared<dielectric>(1.5);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.489, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     camera cam;
@@ -26,9 +26,14 @@ int main() {
     cam.render(world); // this is done to remove const warnings in CLion
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 1920;
-    cam.samples_per_pixel = 100;
+    cam.image_width  = 400;
+    cam.samples_per_pixel = 1;
     cam.max_depth = 30;
+
+    cam.fov = 50;
+    cam.lookat = point3(0, 0, 1);
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.vup = vec3(0, 1 ,0);
 
     cam.render(world);
 }
